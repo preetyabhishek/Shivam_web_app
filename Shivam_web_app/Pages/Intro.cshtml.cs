7a09e73effb7812181e050d1adb4563575a6f4e1
jagga90 <109704875+jagga90@users.noreply.github.com>
2022-07-21 21:38:17 -0400

    Add project files.

---
+++ Shivam_web_app/Pages/Intro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shivam_web_app.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
